8b516c357c97a7cde00b168f32cce22977560d0e
Inovah <oliverpetersenmandalia@gmail.com>
2023-09-22 12:49:34 +0200

    Update main.js
    Kommentarer :)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var jsonObj = {"myObject": [
+//Array af hoodie produkter
+var jsonObj = {"hoodies": [
     {
     title: '350',
     id: "1",
@@ -42,13 +43,19 @@ var jsonObj = {"myObject": [
 
 ]
 }
-var count = Object.keys(jsonObj.myObject).length
-//alert(count);
-var container= document.getElementById('hoodieProdukter');
+//Tjekker hvor mange produkter der er
+var count = Object.keys(jsonObj.hoodies).length
+
+//Finder html elementet det skal være i og giver den en variable via id
+var container = document.getElementById('hoodieProdukter');
+//for loop som starter ved 0 indtil i er stører end mængden af produkter
 for(var i=0;i<count;i++){
-    //console.log(jsonObj.myObject[i]);
-    var obj= jsonObj.myObject[i];
+ 
+    //Laver en variable for hvert produkt
+    var obj = jsonObj.hoodies[i];
+    //laver prisen om til en variable (nemmere hvis den skal bruges meget senere)
     var price = obj.title;
+    //tilføjer hvert produkt til HTML elementet som den hentede id fra tidligere
     container.innerHTML
     +=price;
 }
