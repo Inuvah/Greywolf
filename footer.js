072572445e9839ce5e14a55c5dd2d8f5446e9f01
Inovah <72158807+Inovalia@users.noreply.github.com>
2023-09-22 09:39:55 +0200

    Footer og header fikset
    Mangler lige styling til at passe med figma design og lidt positionering :)

---
+++ footer.js
@@ -0,0 +1,11 @@
+function kontaktFunction() {
+    document.getElementById("kontaktInfo").classList.toggle("show");
+}
+
+function helpFunction() {
+    document.getElementById("helpInfo").classList.toggle("show");
+}
+
+function linkFunction() {
+    document.getElementById("linkInfo").classList.toggle("show");
+}
