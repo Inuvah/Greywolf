2d96e50bfd529e4facc88944339d855c6ad7af48
Inovah <oliverpetersenmandalia@gmail.com>
2023-09-27 12:35:39 +0200

    Se flere farver knap
    På produkter

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var jsonObj = {"hoodies": [
 {
     title: '399 DKK',
     id: "4",
-    imgUrl:'img/HoodieGrey.png',
+    imgUrl:'',
     color:'Hoodie: Grå',
 },
 
@@ -33,18 +33,28 @@ var count = Object.keys(jsonObj.hoodies).length
 
 //Finder html elementet det skal være i og giver den en variable via id
 var container = document.getElementById('hoodieProdukter');
+
 //for loop som starter ved 0 indtil i er stører end mængden af produkter hvis den ikke er i + 1
 for(var i=0;i<count;i++){
  
     //Laver en variable for hvert produkt for hver gang i ændres
     var obj = jsonObj.hoodies[i];
+
     //laver prisen om til en variable (nemmere hvis den skal bruges meget senere)
     var price = obj.title;
-
     var color = obj.color;
+
     //constant variable til billedernes container der efter laver den endnu en div for hvert produkt (i) og giver den en class
     const imgBox = document.createElement('div');
     imgBox.setAttribute('class', 'imgBox');
+
+    //se flere farver knap................. AAAAAHHHHH
+    const colorMore = document.createElement('div');
+    colorMore.setAttribute('class', 'colorMore');
+    const colorMoreA = document.createElement('a');
+    colorMoreA.setAttribute('class', 'colorMoreA');
+    colorMoreA.href = "#";
+    colorMoreA.textContent += "Se flere farver";
 
     //Laver table som fordeler texten under produkt i deres egen box side by side
     const textBox = document.createElement('table');
@@ -53,6 +63,7 @@ for(var i=0;i<count;i++){
     //2 elementer i tabelen øverst og nederst de ligger sig på en linje ned ad
     const textBoxRight = document.createElement('tr');
     const textBoxLeft = document.createElement('tr');
+
     //tilføjer hvert produkt billede til HTML elementet som den hentede id fra tidligere (id=hoodieProdukter som er en div)
     const img = document.createElement('img');
     img.setAttribute('class', 'produktImg')
@@ -73,9 +84,11 @@ for(var i=0;i<count;i++){
     const sizes = document.createElement('td');
     sizes.textContent += "XS  |  S  |  M  |  L  |  XL";
 
-    //container = div hoodieProdukter tilføjer billeder osv som børn til denne div altså de ligger nu inde i min div box
+    //container = div(hoodieProdukter). Tilføjer billeder osv som børn til denne div altså de ligger nu inde i min div box
     container.appendChild(imgBox);
     imgBox.appendChild(img);
+    imgBox.appendChild(colorMore);
+    colorMore.appendChild(colorMoreA);
     imgBox.appendChild(textBox);
     textBox.appendChild(textBoxLeft);
     textBox.appendChild(textBoxRight);
